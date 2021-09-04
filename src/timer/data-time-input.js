a0041687a171f83564cc0969ada736cc3df54e5d
Veronika Mikhotina <mihotinaw@gmail.com>
2021-09-04 19:05:43 +0300

    fix

--- src/timer/data-time-input.js
+++ src/timer/data-time-input.js
@@ -1,8 +1,6 @@
 //------------Проверка на введение значения в input--(2)--------
 //--------------------------------------------------------------
 
-
-
 export function dataTimeInput(timerInput) {
 
    if (timerInput[0].value > 0 && timerInput[0].value < 60 || timerInput[1].value > 0 && timerInput[1].value < 60 || timerInput[2].value > 0 && timerInput[2].value < 60) {
